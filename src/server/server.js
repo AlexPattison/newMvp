adbd1dd18d1778014dc1af3d07dc4fc70f86d328
Alex Pattison <alexlpattison@gmail.com>
2016-08-30 17:52:30 -0700

    About to try hacky solution to books problem

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 var path = require('path')
+var mongoose = require('../db/mongooseTest.js')
 
 app.use(express.static(__dirname + '/../client/'))
 
-app.listen(3000, function() {
+app.listen(5000, function() {
   console.log('The server is running dude!!!!')
 })
